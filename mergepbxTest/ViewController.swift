fea5bea4fb86a5c1959e18c460dac15a3233e234
George Zinyakov <gzinyakov@alfabank.ru>
2017-02-12 16:10:20 +0300

    Added project

---
+++ mergepbxTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mergepbxTest
+//
+//  Created by George Zinyakov on 2/12/17.
+//  Copyright © 2017 George Zinyakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
